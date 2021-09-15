1afa3ccc9cd980681853d6f4ce44b870972d4c3f
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-09-15 23:56:12 +0530

    addProject -fix

--- src/components/AddProjects.js
+++ src/components/AddProjects.js
@@ -19,7 +19,7 @@ export const AddProject = ({ shouldShow = false }) => {
       .add({
         projectId,
         name: projectName,
-        userId: 'jlIFXIwyAL3tzHMtzRbw',
+        userId: 'sdfasfsdfdfasfdsfa',
       })
       .then(() => {
         setProjects([...projects]);
